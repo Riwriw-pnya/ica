f489b38ad682dea723e6b0f862f2c45b0354d232
karlinna <karlinayuw@gmail.com>
2026-09-16 12:33:18 +0700

    fix : urutan sidebar, layout store

--- app/cattery/store/page.tsx
+++ app/cattery/store/page.tsx
@@ -1,5 +1,13 @@
 import StorePage from "@/components/store/StorePage";
 
 export default function CatteryStorePage() {
-  return <StorePage />;
+  return (
+    <div className="min-h-screen bg-[#F8F6F2] text-[#2d2825] font-sans">
+      <main className="p-4 sm:p-6 lg:p-8 space-y-6 max-w-5xl mx-auto">
+        <div className="space-y-1">
+          <StorePage />;
+        </div>
+      </main>
+    </div>
+  )
 }
